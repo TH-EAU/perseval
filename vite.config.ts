4a58796b0ef21dd0e50302c1a53cf512386639af
NICOLAS Théau <theau.nicolas@esiea.fr>
2024-10-11 09:58:53 +0200

    chore: modifybase

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,6 @@ export default defineConfig(({ mode }) => {
       "process.env.REACT_APP_API_URL": JSON.stringify(env.REACT_APP_API_URL),
     },
     plugins: [react()],
+    base: "{repName}",
   };
 });

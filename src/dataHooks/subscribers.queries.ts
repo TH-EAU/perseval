d431311a59d2b2d3628fccfcbd7e75be5c2e30b7
NICOLAS Théau <theau.nicolas@esiea.fr>
2024-10-11 11:14:14 +0200

    Updates

--- src/dataHooks/subscribers.queries.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "react-query";
-import { getSubscribersByProject } from "../services/Subscriber.service";
-
-export const useProjectSubscribers = (projectId: string | undefined) => {
-  return useQuery("projectSubs", () => getSubscribersByProject(projectId));
-};
